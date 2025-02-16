204dddfcb4ec9e74056c99b4abf4c8d92a76ba0b
justnunoo <justnunoo@gmail.com>
2025-02-16 11:52:33 +0000

    minor changes (responsiveness)

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import MissionVisionSection from "@/components/MissionVisionSection";
 
@@ -5,7 +6,7 @@ import RegistrationSections from "@/components/RegistrationSection";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen max-w-screen bg-gray-50 overflow-x-hidden">
       {/* Hero Section */}
       <HeroSection />
       <MissionVisionSection />
@@ -19,14 +20,3 @@ export default function Home() {
 }
 
 
-// Footer Component
-const Footer = () => (
-  <footer className="py-8 bg-gray-50 text-center text-sm text-gray-500">
-    &copy; {new Date().getFullYear()} IntoWine Consult. All rights reserved.
-    <br />
-    Contact us:{" "}
-    <a href="mailto:info@intowineconsult.com" className="text-indigo-600 hover:underline">
-      info@intowineconsult.com
-    </a>
-  </footer>
-);
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="py-8 bg-gray-50 text-center text-sm text-gray-500">
+      &copy; {new Date().getFullYear()} IntoWine Consult. All rights reserved.
+      <br />
+      Contact us:{" "}
+      <a href="mailto:info@intowineconsult.com" className="text-indigo-600 hover:underline">
+        info@intowineconsult.com
+      </a>
+    </footer>
+  )
+}
