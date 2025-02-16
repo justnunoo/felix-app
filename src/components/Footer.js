26fab64ed60384a0613eef1e80ebcdd6677f4ee8
justnunoo <justnunoo@gmail.com>
2025-02-16 21:59:20 +0000

    UI changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="py-8 bg-gray-50 text-center text-sm text-gray-500">
+    <footer id='contact' className="py-8 bg-gray-50 text-center text-sm text-gray-500">
       &copy; {new Date().getFullYear()} IntoWine Consult. All rights reserved.
       <br />
       Contact us:{" "}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -174,14 +174,14 @@ export default function Navbar() {
           {/* Navigation Links (Desktop) */}
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
-              {["Home", "About Us", "Services", "Contact"].map((item, index) => (
+              {["Home", "Services", "About Us", "Contact"].map((item, index) => (
                 <motion.div
                   key={index}
                   whileHover={{ scale: 1.1 }}
                   transition={{ duration: 0.3 }}
                 >
                   <Link
-                    href={item === "Home" ? "/" : `/${item.toLowerCase().replace(" ", "")}`}
+                    href={item === "Home" ? "/" : `#${item.toLowerCase().replace(" ", "")}`}
                     className="hover:text-indigo-200 transition duration-300 px-3 py-2 rounded-md text-sm font-medium"
                   >
                     {item}
@@ -241,14 +241,14 @@ export default function Navbar() {
             transition={{ duration: 0.5, ease: "easeOut" }}
           >
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              {["Home", "About Us", "Services", "Contact"].map((item, index) => (
+              {["Home", "Services", "About Us", "Contact"].map((item, index) => (
                 <motion.div
                   key={index}
                   whileHover={{ scale: 1.05 }}
                   transition={{ duration: 0.3 }}
                 >
                   <Link
-                    href={item === "Home" ? "/" : `/${item.toLowerCase().replace(" ", "")}`}
+                    href={item === "Home" ? "/" : `#${item.toLowerCase().replace(" ", "")}`}
                     className="hover:bg-indigo-600 block px-3 py-2 rounded-md text-base font-medium"
                   >
                     {item}
